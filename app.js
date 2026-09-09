f503fe617471a8253f599138914d3a062fe98f65
heydikshit <141320579+heydikshit@users.noreply.github.com>
2026-09-09 15:05:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var server = require('http').Server(app);
 port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send("Hello World!!! Node Sample App is up and running!");
+    res.send("Hello World!!! Node Sample App is up and running! This is to check the automation");
 });
 
 app.get('/ping', (req, res) => {
